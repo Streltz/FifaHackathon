a1ef88b7975f82a233395896f9720d2a9d62ad2d
Mike Streltsoff <MStreltsoff@gmail.com>
2018-07-03 10:41:17 -0400

    completed skeleton for App

--- fullstack_template/static/js/app.jsx
+++ fullstack_template/static/js/app.jsx
@@ -1,12 +1,29 @@
-
 import React from "react";
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
 
+require('../css/fullstack.css');
+var $ = require('jquery');
 
-class App extends React.Component {
-  render () {
-    return <p> {alert("The winners of the 2018 World Cup Championship will be team") }</p>;
-  }
+import HeaderBackgroundImage from '../images/header.jpg';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    addHeaderImg() {
+        let headerBg = new Image();
+        headerBg.src = HeaderBackgroundImage;
+    }
+
+    render () {
+        return (
+            <PageHeader>
+                <div className='header-contents'>
+                {this.addHeaderImg()}
+                <Hello name='Rimini' />
+                </div>
+            </PageHeader>
+        );
+    }
 }
-
-
-export default App;
