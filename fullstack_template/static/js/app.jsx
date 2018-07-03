4899c45393ac700c31484520a90196777b853bf5
Mike Streltsoff <MStreltsoff@gmail.com>
2018-07-03 04:08:35 -0400

    React and front-end Skeleton for project

---
+++ fullstack_template/static/js/app.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+
+class App extends React.Component {
+  render () {
+    return <p> {alert("The winners of the 2018 World Cup Championship will be team") }</p>;
+  }
+}
+
+
+export default App;
